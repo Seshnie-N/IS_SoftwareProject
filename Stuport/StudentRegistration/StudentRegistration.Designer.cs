360b8de37d9cf1abe90697df1f6ae401a6eb6887
Seshnie-N <2081236@students.wits.ac.za>
2020-10-05 13:25:24 +0200

    GUI Added

---
+++ Stuport/StudentRegistration/StudentRegistration.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace StuportApp
+{
+    partial class StudentRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.lblPhoneNo = new System.Windows.Forms.Label();
+            this.txtPhoneNo = new System.Windows.Forms.TextBox();
+            this.lblPassword1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword2 = new System.Windows.Forms.Label();
+            this.txtPasswordConf = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.ttHints = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please Enter The Following Details To Create A #Stuport Account ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(86, 94);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(204, 20);
+            this.txtFName.TabIndex = 1;
+            this.ttHints.SetToolTip(this.txtFName, "Enter Legal First Name. No Nicknames");
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Location = new System.Drawing.Point(83, 78);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(57, 13);
+            this.lblFName.TabIndex = 2;
+            this.lblFName.Text = "First Name";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Location = new System.Drawing.Point(83, 127);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(61, 13);
+            this.lblLName.TabIndex = 4;
+            this.lblLName.Text = "Last Name ";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(86, 194);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(204, 20);
+            this.txtEmail.TabIndex = 3;
+            this.ttHints.SetToolTip(this.txtEmail, "Enter a Valid Email Address");
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(83, 178);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(73, 13);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "Email Address";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(86, 143);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(204, 20);
+            this.txtLName.TabIndex = 5;
+            this.ttHints.SetToolTip(this.txtLName, "Enter Last Name ");
+            // 
+            // lblPhoneNo
+            // 
+            this.lblPhoneNo.AutoSize = true;
+            this.lblPhoneNo.Location = new System.Drawing.Point(83, 226);
+            this.lblPhoneNo.Name = "lblPhoneNo";
+            this.lblPhoneNo.Size = new System.Drawing.Size(84, 13);
+            this.lblPhoneNo.TabIndex = 8;
+            this.lblPhoneNo.Text = "Contact Number";
+            // 
+            // txtPhoneNo
+            // 
+            this.txtPhoneNo.Location = new System.Drawing.Point(86, 242);
+            this.txtPhoneNo.Name = "txtPhoneNo";
+            this.txtPhoneNo.Size = new System.Drawing.Size(204, 20);
+            this.txtPhoneNo.TabIndex = 7;
+            this.ttHints.SetToolTip(this.txtPhoneNo, "Enter a Valid Phone Number");
+            // 
+            // lblPassword1
+            // 
+            this.lblPassword1.AutoSize = true;
+            this.lblPassword1.Location = new System.Drawing.Point(83, 276);
+            this.lblPassword1.Name = "lblPassword1";
+            this.lblPassword1.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword1.TabIndex = 10;
+            this.lblPassword1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(86, 292);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(204, 20);
+            this.txtPassword.TabIndex = 9;
+            this.ttHints.SetToolTip(this.txtPassword, "Password must be 8 or more characters. Must contain an uppercase character and a " +
+        "lowercase character.");
+            // 
+            // lblPassword2
+            // 
+            this.lblPassword2.AutoSize = true;
+            this.lblPassword2.Location = new System.Drawing.Point(83, 329);
+            this.lblPassword2.Name = "lblPassword2";
+            this.lblPassword2.Size = new System.Drawing.Size(91, 13);
+            this.lblPassword2.TabIndex = 12;
+            this.lblPassword2.Text = "Confirm Password";
+            this.lblPassword2.Click += new System.EventHandler(this.lblPassword2_Click);
+            // 
+            // txtPasswordConf
+            // 
+            this.txtPasswordConf.Location = new System.Drawing.Point(86, 345);
+            this.txtPasswordConf.Name = "txtPasswordConf";
+            this.txtPasswordConf.Size = new System.Drawing.Size(204, 20);
+            this.txtPasswordConf.TabIndex = 11;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(135, 385);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(100, 23);
+            this.btnRegister.TabIndex = 13;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(135, 414);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 23);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // StudentRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 469);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lblPassword2);
+            this.Controls.Add(this.txtPasswordConf);
+            this.Controls.Add(this.lblPassword1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPhoneNo);
+            this.Controls.Add(this.txtPhoneNo);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentRegistration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label lblPhoneNo;
+        private System.Windows.Forms.TextBox txtPhoneNo;
+        private System.Windows.Forms.Label lblPassword1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword2;
+        private System.Windows.Forms.TextBox txtPasswordConf;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ToolTip ttHints;
+    }
+}
