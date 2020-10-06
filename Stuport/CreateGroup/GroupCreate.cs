363c5a1823906e108e458af524936069cee15ce3
Robert Schwarz <robert@a2pay.co.za>
2020-10-06 15:32:19 +0200

    Added Login & Create Group

---
+++ Stuport/CreateGroup/GroupCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stuport.CreateGroup
+{
+    public partial class GroupCreate : Form
+    {
+        public GroupCreate()
+        {
+            InitializeComponent();
+        }
+    }
+}
