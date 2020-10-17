9631b232dd7a164a8a9af81b6f15e35e0980b8ab
RushilPatel0703 <30756824+RushilPatel0703@users.noreply.github.com>
2020-10-17 14:51:35 +0200

    Update StudentProfile.Designer.cs
    updated

--- Stuport/StudentProfile/StudentProfile.Designer.cs
+++ Stuport/StudentProfile/StudentProfile.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Stuport.StudentMenu
+﻿namespace Stuport
 {
     partial class StudentProfile
     {
@@ -28,140 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.profilelbl = new System.Windows.Forms.Label();
-            this.updatebtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.stdNumtb = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.titlelbl = new System.Windows.Forms.Label();
+            this.stdnumlbl = new System.Windows.Forms.Label();
+            this.fnlbl = new System.Windows.Forms.Label();
+            this.lnlbl = new System.Windows.Forms.Label();
+            this.emaillbl = new System.Windows.Forms.Label();
+            this.phnumlbl = new System.Windows.Forms.Label();
+            this.stdnumtb = new System.Windows.Forms.TextBox();
             this.fntb = new System.Windows.Forms.TextBox();
             this.lntb = new System.Windows.Forms.TextBox();
             this.emailtb = new System.Windows.Forms.TextBox();
             this.phnumtb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backbtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.stuportDatabaseDataSet = new Stuport.StuportDatabaseDataSet();
+            this.stuportDatabaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stuportDatabaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stuportDatabaseDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // profilelbl
+            // titlelbl
             // 
-            this.profilelbl.AutoSize = true;
-            this.profilelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.profilelbl.Location = new System.Drawing.Point(132, 9);
-            this.profilelbl.Name = "profilelbl";
-            this.profilelbl.Size = new System.Drawing.Size(107, 24);
-            this.profilelbl.TabIndex = 0;
-            this.profilelbl.Text = "Your Profile";
+            this.titlelbl.AutoSize = true;
+            this.titlelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titlelbl.Location = new System.Drawing.Point(131, 9);
+            this.titlelbl.Name = "titlelbl";
+            this.titlelbl.Size = new System.Drawing.Size(62, 24);
+            this.titlelbl.TabIndex = 0;
+            this.titlelbl.Text = "Profile";
             // 
-            // updatebtn
+            // stdnumlbl
             // 
-            this.updatebtn.Location = new System.Drawing.Point(254, 315);
-            this.updatebtn.Name = "updatebtn";
-            this.updatebtn.Size = new System.Drawing.Size(88, 23);
-            this.updatebtn.TabIndex = 1;
-            this.updatebtn.Text = "Update Profile";
-            this.updatebtn.UseVisualStyleBackColor = true;
-            this.updatebtn.Click += new System.EventHandler(this.updatebtn_Click);
+            this.stdnumlbl.AutoSize = true;
+            this.stdnumlbl.Location = new System.Drawing.Point(42, 75);
+            this.stdnumlbl.Name = "stdnumlbl";
+            this.stdnumlbl.Size = new System.Drawing.Size(67, 13);
+            this.stdnumlbl.TabIndex = 1;
+            this.stdnumlbl.Text = "Student No.:";
             // 
-            // label1
+            // fnlbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 76);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Student Number:";
+            this.fnlbl.AutoSize = true;
+            this.fnlbl.Location = new System.Drawing.Point(42, 114);
+            this.fnlbl.Name = "fnlbl";
+            this.fnlbl.Size = new System.Drawing.Size(57, 13);
+            this.fnlbl.TabIndex = 2;
+            this.fnlbl.Text = "First Name";
             // 
-            // label2
+            // lnlbl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 121);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "First Name:";
+            this.lnlbl.AutoSize = true;
+            this.lnlbl.Location = new System.Drawing.Point(42, 149);
+            this.lnlbl.Name = "lnlbl";
+            this.lnlbl.Size = new System.Drawing.Size(58, 13);
+            this.lnlbl.TabIndex = 3;
+            this.lnlbl.Text = "Last Name";
             // 
-            // label3
+            // emaillbl
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(52, 167);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Last Name:";
+            this.emaillbl.AutoSize = true;
+            this.emaillbl.Location = new System.Drawing.Point(42, 184);
+            this.emaillbl.Name = "emaillbl";
+            this.emaillbl.Size = new System.Drawing.Size(32, 13);
+            this.emaillbl.TabIndex = 4;
+            this.emaillbl.Text = "Email";
             // 
-            // label4
+            // phnumlbl
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(52, 212);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Email:";
+            this.phnumlbl.AutoSize = true;
+            this.phnumlbl.Location = new System.Drawing.Point(42, 222);
+            this.phnumlbl.Name = "phnumlbl";
+            this.phnumlbl.Size = new System.Drawing.Size(61, 13);
+            this.phnumlbl.TabIndex = 5;
+            this.phnumlbl.Text = "Phone No.:";
             // 
-            // label5
+            // stdnumtb
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 256);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Phone No.:";
-            // 
-            // stdNumtb
-            // 
-            this.stdNumtb.Location = new System.Drawing.Point(182, 68);
-            this.stdNumtb.Name = "stdNumtb";
-            this.stdNumtb.Size = new System.Drawing.Size(100, 20);
-            this.stdNumtb.TabIndex = 12;
-            this.stdNumtb.TextChanged += new System.EventHandler(this.stdNumtb_TextChanged);
+            this.stdnumtb.Location = new System.Drawing.Point(173, 67);
+            this.stdnumtb.Name = "stdnumtb";
+            this.stdnumtb.Size = new System.Drawing.Size(100, 20);
+            this.stdnumtb.TabIndex = 6;
             // 
             // fntb
             // 
-            this.fntb.Location = new System.Drawing.Point(182, 114);
+            this.fntb.Location = new System.Drawing.Point(173, 107);
             this.fntb.Name = "fntb";
             this.fntb.Size = new System.Drawing.Size(100, 20);
-            this.fntb.TabIndex = 13;
+            this.fntb.TabIndex = 7;
             // 
             // lntb
             // 
-            this.lntb.Location = new System.Drawing.Point(182, 160);
+            this.lntb.Location = new System.Drawing.Point(173, 142);
             this.lntb.Name = "lntb";
             this.lntb.Size = new System.Drawing.Size(100, 20);
-            this.lntb.TabIndex = 14;
+            this.lntb.TabIndex = 8;
             // 
             // emailtb
             // 
-            this.emailtb.Location = new System.Drawing.Point(182, 205);
+            this.emailtb.Location = new System.Drawing.Point(173, 177);
             this.emailtb.Name = "emailtb";
             this.emailtb.Size = new System.Drawing.Size(100, 20);
-            this.emailtb.TabIndex = 15;
+            this.emailtb.TabIndex = 9;
             // 
             // phnumtb
             // 
-            this.phnumtb.Location = new System.Drawing.Point(182, 249);
+            this.phnumtb.Location = new System.Drawing.Point(173, 215);
             this.phnumtb.Name = "phnumtb";
             this.phnumtb.Size = new System.Drawing.Size(100, 20);
-            this.phnumtb.TabIndex = 16;
+            this.phnumtb.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Save Changes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backbtn
+            // 
+            this.backbtn.Location = new System.Drawing.Point(45, 314);
+            this.backbtn.Name = "backbtn";
+            this.backbtn.Size = new System.Drawing.Size(95, 23);
+            this.backbtn.TabIndex = 12;
+            this.backbtn.Text = "Back";
+            this.backbtn.UseVisualStyleBackColor = true;
+            this.backbtn.Click += new System.EventHandler(this.backbtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(316, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(359, 150);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // stuportDatabaseDataSet
+            // 
+            this.stuportDatabaseDataSet.DataSetName = "StuportDatabaseDataSet";
+            this.stuportDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // stuportDatabaseDataSetBindingSource
+            // 
+            this.stuportDatabaseDataSetBindingSource.DataSource = this.stuportDatabaseDataSet;
+            this.stuportDatabaseDataSetBindingSource.Position = 0;
             // 
             // StudentProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 350);
+            this.ClientSize = new System.Drawing.Size(717, 349);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.backbtn);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.phnumtb);
             this.Controls.Add(this.emailtb);
             this.Controls.Add(this.lntb);
             this.Controls.Add(this.fntb);
-            this.Controls.Add(this.stdNumtb);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.updatebtn);
-            this.Controls.Add(this.profilelbl);
+            this.Controls.Add(this.stdnumtb);
+            this.Controls.Add(this.phnumlbl);
+            this.Controls.Add(this.emaillbl);
+            this.Controls.Add(this.lnlbl);
+            this.Controls.Add(this.fnlbl);
+            this.Controls.Add(this.stdnumlbl);
+            this.Controls.Add(this.titlelbl);
             this.Name = "StudentProfile";
             this.Text = "StudentProfile";
+            this.Load += new System.EventHandler(this.StudentProfile_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stuportDatabaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stuportDatabaseDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,17 +210,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label profilelbl;
-        private System.Windows.Forms.Button updatebtn;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox stdNumtb;
+        private System.Windows.Forms.Label titlelbl;
+        private System.Windows.Forms.Label stdnumlbl;
+        private System.Windows.Forms.Label fnlbl;
+        private System.Windows.Forms.Label lnlbl;
+        private System.Windows.Forms.Label emaillbl;
+        private System.Windows.Forms.Label phnumlbl;
+        private System.Windows.Forms.TextBox stdnumtb;
         private System.Windows.Forms.TextBox fntb;
         private System.Windows.Forms.TextBox lntb;
         private System.Windows.Forms.TextBox emailtb;
         private System.Windows.Forms.TextBox phnumtb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button backbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource stuportDatabaseDataSetBindingSource;
+        private StuportDatabaseDataSet stuportDatabaseDataSet;
     }
 }
