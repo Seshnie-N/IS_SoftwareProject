372fc5e35594bc9176d0eb2b1ae7585e8725e6f6
james thackeray <jsthackeray@outlook.com>
2020-10-27 17:08:49 +0200

    make appoint changed

---
+++ Stuport/GroupPage/GroupPage.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Stuport.GroupPage
+{
+    partial class GroupPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGroups = new System.Windows.Forms.Label();
+            this.dgvGroups = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnJoinGroup = new System.Windows.Forms.Button();
+            this.bntRequestGroup = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGroups)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGroups
+            // 
+            this.lblGroups.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGroups.AutoSize = true;
+            this.lblGroups.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGroups.Location = new System.Drawing.Point(277, 9);
+            this.lblGroups.Name = "lblGroups";
+            this.lblGroups.Size = new System.Drawing.Size(262, 31);
+            this.lblGroups.TabIndex = 6;
+            this.lblGroups.Text = "Group Appointments";
+            // 
+            // dgvGroups
+            // 
+            this.dgvGroups.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGroups.Location = new System.Drawing.Point(12, 46);
+            this.dgvGroups.Name = "dgvGroups";
+            this.dgvGroups.Size = new System.Drawing.Size(776, 358);
+            this.dgvGroups.TabIndex = 7;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 415);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(123, 23);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnJoinGroup
+            // 
+            this.btnJoinGroup.Location = new System.Drawing.Point(665, 415);
+            this.btnJoinGroup.Name = "btnJoinGroup";
+            this.btnJoinGroup.Size = new System.Drawing.Size(123, 23);
+            this.btnJoinGroup.TabIndex = 10;
+            this.btnJoinGroup.Text = "Join Group";
+            this.btnJoinGroup.UseVisualStyleBackColor = true;
+            // 
+            // bntRequestGroup
+            // 
+            this.bntRequestGroup.Location = new System.Drawing.Point(496, 415);
+            this.bntRequestGroup.Name = "bntRequestGroup";
+            this.bntRequestGroup.Size = new System.Drawing.Size(163, 23);
+            this.bntRequestGroup.TabIndex = 11;
+            this.bntRequestGroup.Text = "Request Group Appointment";
+            this.bntRequestGroup.UseVisualStyleBackColor = true;
+            // 
+            // GroupPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bntRequestGroup);
+            this.Controls.Add(this.btnJoinGroup);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.dgvGroups);
+            this.Controls.Add(this.lblGroups);
+            this.Name = "GroupPage";
+            this.Text = "GroupPage";
+            this.Load += new System.EventHandler(this.GroupPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGroups)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGroups;
+        private System.Windows.Forms.DataGridView dgvGroups;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnJoinGroup;
+        private System.Windows.Forms.Button bntRequestGroup;
+    }
+}
