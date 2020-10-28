f9d72a5643ea4448e2a74bbc96c9e50f38c98598
RushilPatel0703 <30756824+RushilPatel0703@users.noreply.github.com>
2020-10-28 23:17:36 +0200

    Student Profile page design done

--- Stuport/StudentProfile/StudentProfile.cs
+++ Stuport/StudentProfile/StudentProfile.cs
@@ -81,5 +81,15 @@ namespace Stuport
 
 
         }
+
+        private void exitlbl_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimizelbl_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
     }
 }
