a111dd2a21bae4d02db48897e2f32267735f6179
Mohammad Uppel <2090704@students.wits.ac.za>
2020-10-28 12:46:37 +0200

    password box *

--- Stuport/UpdateStudent/UpdateStudent.Designer.cs
+++ Stuport/UpdateStudent/UpdateStudent.Designer.cs
@@ -81,6 +81,7 @@
             // 
             this.txtPassword.Location = new System.Drawing.Point(367, 296);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(204, 20);
             this.txtPassword.TabIndex = 4;
             // 
